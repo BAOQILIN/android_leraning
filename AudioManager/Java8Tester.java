8137ec6bd7d9ae57319a48ad148579d370647907
qilin.bao <2198658867@qq.com>
2021-02-10 14:19:19 +0800

    add test folder AudioManager

---
+++ AudioManager/Java8Tester.java
@@ -0,0 +1,10 @@
+class Java8Tester{
+    public static void main(String[] args){
+        Java8Tester mm = new Java8Tester();
+        System.out.println(mm.fun());
+    }
+        
+    int fun(){
+        return 20;
+    }
+    }
